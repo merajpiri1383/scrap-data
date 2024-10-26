38a73b30fc2b6bfa2dbd7e8a65547c5815dda3ce
meraj <merajpiri83@gmail.com>
2024-10-26 15:05:24 +0330

    complete 30 code

--- info.py
+++ info.py
@@ -12,73 +12,93 @@ driver = webdriver.Chrome()
 def get_info_30 (href ,date) : 
     driver.get(href)
 
+    most_sell = {
+        "product" : 0,
+        "name" : None
+    }
+
+     
     
     
-    # try : 
+    try : 
         
-    #     div_tags = driver.find_elements(By.CLASS_NAME,"varios")
-    #     sal_mali = div_tags[5].text.strip() if div_tags else None
-    #     saramayed_sabt = div_tags[1].text.strip() if div_tags else 0
-    #     namad = div_tags[2].text.strip() if div_tags else None
-    #     ghozresh_mahianeh = driver.find_element(By.TAG_NAME,"bdo").text.strip()
-    #     table = driver.find_elements(By.TAG_NAME,"table")[0]
-    #     trs = table.find_elements(By.TAG_NAME,"tr")
-    #     last_tr = trs[-1]
-    #     tds = last_tr.find_elements(By.TAG_NAME,"td")
-    #     jam_phorosh_dahkeli = 0
-    #     product_row = 0
+        div_tags = driver.find_elements(By.CLASS_NAME,"varios")
+        sal_mali = div_tags[5].text.strip() if div_tags else None
+        saramayed_sabt = div_tags[1].text.strip() if div_tags else 0
+        namad = div_tags[2].text.strip() if div_tags else None
+        ghozresh_mahianeh = driver.find_element(By.TAG_NAME,"bdo").text.strip()
+        table = driver.find_elements(By.TAG_NAME,"table")[0]
+        trs = table.find_elements(By.TAG_NAME,"tr")
+        last_tr = trs[-1]
+        tds = last_tr.find_elements(By.TAG_NAME,"td")
+        jam_phorosh_dahkeli = 0
 
-    #     try : 
-    #         for index,tr in enumerate(trs) : 
-    #             if "جمع فروش داخلی" in tr.text : 
-    #                 product_row = trs[index - 2]
-    #                 jam_phorosh_dahkeli = tr.find_elements(By.XPATH,"./td")[16].text.strip()
-    #     except : 
-    #         jam_phorosh_dahkeli = 0
+        for index,tr in enumerate(trs) : 
+            tds = tr.find_elements(By.XPATH,".//td")
+            if tds : 
+                if "جمع فروش داخلی" in tds[0].text.strip()  : 
+                    break
+                elif index < 3 :
+                    continue
+                else : 
+                    try : 
+                        result = int(str(tds[16].text.strip().replace(",","")).translate(english_translate))
+                        if most_sell["product"] < result : 
+                            most_sell = {
+                                "product" : result,
+                                "name" : tds[0].text.strip()
+                            }
+                    except : 
+                        pass
+
+        try : 
+            for index,tr in enumerate(trs) : 
+                if "جمع فروش داخلی" in tr.text : 
+                    jam_phorosh_dahkeli = tr.find_elements(By.XPATH,"./td")[16].text.strip()
+        except : 
+            jam_phorosh_dahkeli = 0
 
 
-    #     mahsol = str(product_row.find_elements(By.XPATH,"./td")[16].text.strip()).translate(english_translate)
+
+        try : 
+            yek_mah_montahi = tds[12].text.strip() 
+        except : 
+            yek_mah_montahi = 0
         
+        try : 
+            as_ebteday_sal_1 = tds[16].text.strip()
+        except : 
+            as_ebteday_sal_1 = 0
+
+        try : 
+            as_ebteday_sal_2 = driver.execute_script("return arguments[0].innerText",tds[-2])
+        except : 
+            as_ebteday_sal_2 = 0
+
+        date,time = date.split(" ")
 
 
-    #     try : 
-    #         yek_mah_montahi = tds[12].text.strip() 
-    #     except : 
-    #         yek_mah_montahi = 0
-        
-    #     try : 
-    #         as_ebteday_sal_1 = tds[16].text.strip()
-    #     except : 
-    #         as_ebteday_sal_1 = 0
-
-    #     try : 
-    #         as_ebteday_sal_2 = driver.execute_script("return arguments[0].innerText",tds[-2])
-    #     except : 
-    #         as_ebteday_sal_2 = 0
-
-    #     date,time = date.split(" ")
-
-
-    #     return (
-    #         namad,
-    #         str(date).translate(english_translate),
-    #         str(time).translate(english_translate),
-    #         sal_mali,
-    #         saramayed_sabt,
-    #         ghozresh_mahianeh,
-    #         yek_mah_montahi.translate(english_translate),
-    #         as_ebteday_sal_1.translate(english_translate),
-    #         str(as_ebteday_sal_2).translate(english_translate),
-    #         str(jam_phorosh_dahkeli).translate(english_translate),
-    #         mahsol,
-    #     )
-    # except : 
-    #     return None
+        return (
+            namad,
+            str(date).translate(english_translate),
+            str(time).translate(english_translate),
+            sal_mali,
+            saramayed_sabt,
+            ghozresh_mahianeh,
+            yek_mah_montahi.translate(english_translate),
+            as_ebteday_sal_1.translate(english_translate),
+            str(as_ebteday_sal_2).translate(english_translate),
+            str(jam_phorosh_dahkeli).translate(english_translate),
+            most_sell["name"],
+        )
+    except : 
+        return None
 
 
 
-result = get_info_30("https://www.codal.ir/Reports/Decision.aspx?LetterSerial=QKQQiUMIOOOObOOOl5boEhnOQQQaQQQcNA%3d%3d&rt=0&let=58&ct=0&ft=-1","f f")
+# result = get_info_30("https://www.codal.ir/Reports/Decision.aspx?LetterSerial=QKQQiUMIOOOObOOOl5boEhnOQQQaQQQcNA%3d%3d&rt=0&let=58&ct=0&ft=-1","f f")
 
+# print(result)
 
 def get_info_code_31 (href,date) : 
 
